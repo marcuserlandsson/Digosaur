2e08437d8de64223a1b66cd4f0b8e142999c527c
Nahida Islam <37833834+nahidaislam@users.noreply.github.com>
2025-10-10 15:10:55 +0200

    Feat: Working blob tracking
    Each Blob contains information on it's movement from last frame. Blobs do not remember their number, so if blobs 0 and 1 exist, and blob 0 stops existing, blob 1 will get the number blob 0 but still only keep it's own tracking data.

--- src/Digosaur/blob_tracking.gd
+++ src/Digosaur/blob_tracking.gd
@@ -23,18 +23,31 @@ func tracking_function(previous: Array, current: Array) -> void:
 	for i in range(len(current)):
 		var current_point = Vector2(current[i][0], current[i][1])
 		distances_c_to_p[i] = Array([])
-		for j in previous:
-			if !distances_p_to_c[j]:
+		for j in range(len(previous)):
+			if !distances_p_to_c.has(j):
 				distances_p_to_c[j] = Array([])
-			var previous_point = Vector2(j[0], j[1])
+			var previous_point = Vector2(previous[j][0], previous[j][1])
 			var distance = (current_point - previous_point).length()
 			distances_c_to_p[i].append(distance)
 			distances_p_to_c[j].append(distance)
 	for i in distances_c_to_p.keys():
 		var shortest = INF
-		for j in range(len(distances_c_to_p[i])):
-			if distances_c_to_p[i][j] < shortest and distances_c_to_p[i][j] <= threshold and distances_c_to_p[i][j] < min(distances_p_to_c[j]):
-				tracking.append(Array([current[i][0], current[i][1], current[i][2], current[i][3], Vector2(current[i][0] - previous[j][0], current[i][1] - previous[j][1])]))
-			else:
-				tracking.append(Array([current[i][0], current[i][1], current[i][2], current[i][3], "new"]))
-			
+		var best = Array()
+		#print("New touchPoint: ", i)
+		for j in distances_p_to_c.keys():
+			#print("Previous touchPoint: ", j)
+			#print(distances_c_to_p[i][j], " < ", shortest, " = ", distances_c_to_p[i][j] < shortest)
+			#print(distances_c_to_p[i][j], " <= ", threshold, " = ", distances_c_to_p[i][j] <= threshold)
+			#print(distances_c_to_p[i][j], " < ", distances_p_to_c[j].min(), " = ", distances_c_to_p[i][j] <= distances_p_to_c[j].min())
+			#print("All tests: ", distances_c_to_p[i][j] < shortest and distances_c_to_p[i][j] <= threshold and distances_c_to_p[i][j] <= distances_p_to_c[j].min())
+			if distances_c_to_p[i][j] < shortest and distances_c_to_p[i][j] <= threshold and distances_c_to_p[i][j] <= distances_p_to_c[j].min():
+				best = Array([current[i][0], current[i][1], current[i][2], current[i][3], Vector2(current[i][0] - previous[j][0], current[i][1] - previous[j][1])])
+				shortest = distances_c_to_p[i][j]
+				#print("Succeeded, adding: ", Array([current[i][0], current[i][1], current[i][2], current[i][3], Vector2(current[i][0] - previous[j][0], current[i][1] - previous[j][1])]))
+			#else:
+				#tracking.append(Array([current[i][0], current[i][1], current[i][2], current[i][3], Vector2(-1, -1)]))
+				#print("Failed, adding: ", Array([current[i][0], current[i][1], current[i][2], current[i][3], Vector2(-1, -1)]))
+		if shortest == INF:
+			tracking.append(Array([current[i][0], current[i][1], current[i][2], current[i][3], Vector2(-1, -1)]))
+		else:
+			tracking.append(best)
